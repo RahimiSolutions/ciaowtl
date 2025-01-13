dd835cbd3c11ec917102507a77e41109ae7bd8cb
RahimiSolutions <rahimisolutions@gmail.com>
2025-01-13 22:00:02 +0100

    save

---
+++ src/routes/imprint/+page.svelte
@@ -0,0 +1,30 @@
+<h1>Imprint - "Impressum"</h1>
+
+<div class="company-info">
+	<div class="title"><h2>Ciao Logistics</h2></div>
+    <p>street name and number</p>
+    <p>postal code city, Germany</p>
+</div>
+
+<div class="contact-info">
+    <div class="title"><h2>Contact Information</h2></div>
+    <p>Email: <a href="mailto:info@ciao-wtl.com">info@ciao-wtl.com</a></p>
+    <p>Phone: <a href="tel:+494075695565">+49 4075 695565</a></p>
+</div>
+<div class="represented-by">
+    <div class="title"><h2>Represented by</h2></div>
+    <p>Full name ahmed</p>
+    <p>Full name miriam</p>
+</div>
+<div class="ٌregistry-entry">
+    <div class="title"><h2>Registry Entry</h2></div>
+    
+</div>
+<div class="vat">
+    <div class="title"><h2>VAT Identification Number</h2></div>
+
+</div>
+<div class="responsible">
+    <div class="title"><h2>Responsible for Content (§ 55 Abs. 2 RStV):</h2></div>
+
+</div>
